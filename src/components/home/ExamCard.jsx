6129172140d7a9d1e59fb77832938224a2727e73
munna-ahmed-web <web.munnaahmed@gmail.com>
2025-01-31 15:24:51 +0600

    updated

---
+++ src/components/home/ExamCard.jsx
@@ -0,0 +1,5 @@
+const ExamCard = () => {
+  return <div>Exam Card</div>;
+};
+
+export default ExamCard;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,93 +3,101 @@ import useFetchQuery from "../hooks/shared/useFetch";
 import { useState } from "react";
 import CommonWrapper from "../components/CommonWrapper";
 import { categoryData, PerPageData } from "../enums/sortingData";
-
-
+import ExamCard from "../components/home/ExamCard";
 
 const Home = () => {
- const [category, setCategory] = useState("")
-const [page, setPage] = useState(1)
-const [searchQuery, setSearchQuery] = useState('')
-const [perPage, setPerPage] = useState(5)
+  const [category, setCategory] = useState("");
+  const [page, setPage] = useState(1);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [perPage, setPerPage] = useState(5);
 
-const queryParams = {
-  searchTerm: searchQuery,
-  limit: perPage,
-  page: page,
-};
+  const queryParams = {
+    searchTerm: searchQuery,
+    limit: perPage,
+    page: page,
+  };
 
-if (category) {
-  queryParams.category = category;
-}
+  if (category) {
+    queryParams.category = category;
+  }
 
+  const { data, isSuccess, isLoading } = useFetchQuery("jobs", queryParams);
+  const onCategoryChange = (value) => {
+    if (value == "All") {
+      console.log(value);
 
-const { data, isSuccess, isLoading } = useFetchQuery('jobs', queryParams);
- const onCategoryChange = (value ) => {
+      setCategory("");
+    } else {
+      const valueArray = value.split(",");
+      console.log(valueArray);
+      setCategory(valueArray);
+    }
+  };
+  if (isSuccess) {
+    console.log(data);
+  }
 
-if(value == "All"){
-  console.log(value)
+  const OnSearchChnage = (value) => {
+    setPage(1);
+    setSearchQuery(value);
+  };
+  return (
+    <>
+      <CommonWrapper className="flex gap-8">
+        <ExamCard />
+        <Input
+          isClearable
+          onChange={(e) => {
+            OnSearchChnage(e.target.value);
+          }}
+          label="Search"
+          placeholder="Type to search..."
+          radius="lg"
+        />
+        <Select
+          selectionMode="multiple"
+          defaultSelectedKeys={[`All`]}
+          onChange={(e) => {
+            onCategoryChange(e.target.value);
+          }}
+          className="max-w-xs"
+          label="Category"
+        >
+          {categoryData.map((animal) => (
+            <SelectItem key={animal.key}>{animal.label}</SelectItem>
+          ))}
+        </Select>
 
-  setCategory("")
-}else{
- 
-  const valueArray = value.split(',');
-  console.log(valueArray)
-  setCategory(valueArray)
-}
- }
-if(isSuccess){
-  console.log(data)
-}
- 
-const OnSearchChnage= (value)=> {
-  setPage(1)
-setSearchQuery(value)
-
-}
-  return(
-   <>
-   <CommonWrapper className="flex gap-8">
-   <Input
-        isClearable
-        onChange={(e)=> {OnSearchChnage(e.target.value)}}
-        label="Search"
-        placeholder="Type to search..."
-        radius="lg"
-      
-      />
-<Select selectionMode="multiple" defaultSelectedKeys={[`All`]} onChange={(e)=> {onCategoryChange(e.target.value)}} className="max-w-xs" label="Category">
-        {categoryData.map((animal) => (
-          <SelectItem key={animal.key}>{animal.label}</SelectItem>
-        ))}
-      </Select>
-
-
-
-      <Select defaultSelectedKeys={[`${perPage}`]} onChange={(e)=> {setPerPage(e.target.value)}} className="max-w-xs" label="Per Page">
-        {PerPageData.map((animal) => (
-          <SelectItem key={animal.key}>{animal.label}</SelectItem>
-        ))}
-      </Select>
-   </CommonWrapper>
-   <CommonWrapper> 
-    {isLoading && <Spinner />} 
-    {!isLoading &&  isSuccess && <>  Data: {data?.data?.length}</>}
-    </CommonWrapper>
-   <CommonWrapper>  
-   <Pagination
-   
-      page={page}
-    onChange={setPage}
-      classNames={{
-        wrapper: "gap-0 overflow-visible h-8 rounded border border-divider",
-        item: "w-8 h-8 text-small rounded-none bg-transparent",
-        cursor:
-          "bg-gradient-to-b shadow-lg from-default-500 to-default-800 dark:from-default-300 dark:to-default-100 text-white font-bold",
-      }}
-      total={Math.ceil(data?.meta?.total/perPage)}
-    />
-
-    </CommonWrapper>  
+        <Select
+          defaultSelectedKeys={[`${perPage}`]}
+          onChange={(e) => {
+            setPerPage(e.target.value);
+          }}
+          className="max-w-xs"
+          label="Per Page"
+        >
+          {PerPageData.map((animal) => (
+            <SelectItem key={animal.key}>{animal.label}</SelectItem>
+          ))}
+        </Select>
+      </CommonWrapper>
+      <CommonWrapper>
+        {isLoading && <Spinner />}
+        {!isLoading && isSuccess && <> Data: {data?.data?.length}</>}
+      </CommonWrapper>
+      <CommonWrapper>
+        <Pagination
+          page={page}
+          onChange={setPage}
+          classNames={{
+            wrapper: "gap-0 overflow-visible h-8 rounded border border-divider",
+            item: "w-8 h-8 text-small rounded-none bg-transparent",
+            cursor:
+              "bg-gradient-to-b shadow-lg from-default-500 to-default-800 dark:from-default-300 dark:to-default-100 text-white font-bold",
+          }}
+          total={Math.ceil(data?.meta?.total / perPage)}
+        />
+      </CommonWrapper>
     </>
   );
 };
