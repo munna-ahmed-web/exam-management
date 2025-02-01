884d2cb56eddae8c1d8d0b07b8ec4defc809f797
isratahamedsabbir <isratahanedsabbir@gmail.com>
2025-02-01 18:14:01 +0600

    home page - card add

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,105 +1,269 @@
-import { Input, Pagination, Select, SelectItem, Spinner } from "@heroui/react";
-import useFetchQuery from "../hooks/shared/useFetch";
-import { useState } from "react";
+
 import CommonWrapper from "../components/CommonWrapper";
-import { categoryData, PerPageData } from "../enums/sortingData";
-import ExamCard from "../components/home/ExamCard";
+import { Link, Button } from "@heroui/react";
+
+import { Card, CardContent, CardDescription, CardFooter, CardTitle } from "../components/ui/card";
+import seoImg from "../assets/home/seo.svg"
+import itImg from "../assets/home/it.svg"
+import adwordImg from "../assets/home/adwords.svg"
+import marketingImg from "../assets/home/maketing.svg"
+import {Pagination} from "@heroui/react";
+
+
 
 const Home = () => {
-  const [category, setCategory] = useState("");
-  const [page, setPage] = useState(1);
-  const [searchQuery, setSearchQuery] = useState("");
-  const [perPage, setPerPage] = useState(5);
+  const colors = ["primary", "secondary", "success", "warning", "danger"];
 
-  const queryParams = {
-    searchTerm: searchQuery,
-    limit: perPage,
-    page: page,
-  };
-
-  if (category) {
-    queryParams.category = category;
-  }
-
-  const { data, isSuccess, isLoading } = useFetchQuery("jobs", queryParams);
-  const onCategoryChange = (value) => {
-    if (value == "All") {
-      console.log(value);
-
-      setCategory("");
-    } else {
-      const valueArray = value.split(",");
-      console.log(valueArray);
-      setCategory(valueArray);
-    }
-  };
-  if (isSuccess) {
-    console.log(data);
-  }
-
-  const OnSearchChnage = (value) => {
-    setPage(1);
-    setSearchQuery(value);
-  };
   return (
     <>
-      <CommonWrapper className="flex gap-8">
-        <ExamCard />
-        <p>this is new home by eitty</p>
-        <Input
-          isClearable
-          onChange={(e) => {
-            OnSearchChnage(e.target.value);
-          }}
-          label="Search"
-          placeholder="Type to search..."
-          radius="lg"
-        />
-        <Select
-          selectionMode="multiple"
-          defaultSelectedKeys={[`All`]}
-          onChange={(e) => {
-            onCategoryChange(e.target.value);
-          }}
-          className="max-w-xs"
-          label="Category"
-        >
-          {categoryData.map((animal) => (
-            <SelectItem key={animal.key}>{animal.label}</SelectItem>
-          ))}
-        </Select>
+      <CommonWrapper>
+        <h1 className="md:text-4xl text-3xl font-extrabold text-center "> All Question papers </h1>
+      </CommonWrapper>
 
-        <Select
-          defaultSelectedKeys={[`${perPage}`]}
-          onChange={(e) => {
-            setPerPage(e.target.value);
-          }}
-          className="max-w-xs"
-          label="Per Page"
-        >
-          {PerPageData.map((animal) => (
-            <SelectItem key={animal.key}>{animal.label}</SelectItem>
-          ))}
-        </Select>
+      <CommonWrapper className="mt-4 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+        <Card className="hover:shadow-lg md:hover:scale-105 hover:shadow-primary/50 transition-all duration-300 flex flex-col ">
+          <CardContent className="pt-3">
+            <img src={seoImg} alt="" className="w-full h-48 object-cover rounded-t-lg" />
+          </CardContent>
+
+          <CardTitle>
+            <h2 className="p-6">English</h2>
+          </CardTitle>
+
+          <CardDescription>
+            <p className="px-6 text-lg flex-grow">Master the art of communication, literature, and writing skills. Learn grammar, vocabulary, and storytelling techniques to excel in English</p>
+          </CardDescription>
+          <CardContent className="flex gap-2 pt-6 flex-shrink-0">
+            <p>Question: 30 |</p>
+            <p> Time: 60 min.</p>
+          </CardContent>
+          <CardFooter className="flex justify-center">
+            <Button
+              showAnchorIcon
+              as={Link}
+              color="success"
+              href="#"
+              variant="solid"
+            >
+              See More
+            </Button>
+          </CardFooter>
+
+        </Card >
+        <Card className="hover:shadow-lg md:hover:scale-105 hover:shadow-primary/50 transition-all duration-300 flex flex-col">
+          <CardContent className="pt-3">
+            <img src={itImg} alt="" className="w-full h-48 object-cover rounded-t-lg" />
+          </CardContent>
+
+          <CardTitle>
+            <h2 className="p-6">Computer Science</h2>
+          </CardTitle>
+
+          <CardDescription>
+            <p className="px-6 text-lg flex-grow">Dive into programming, algorithms, and software development. Learn coding languages, data structures, and how to build applications.</p>
+          </CardDescription>
+          <CardContent className="flex gap-2 pt-6 flex-shrink-0">
+            <p>Question: 30 |</p>
+            <p> Time: 60 min.</p>
+          </CardContent>
+          <CardFooter className="flex justify-center">
+            <Button
+              showAnchorIcon
+              as={Link}
+              color="success"
+              href="#"
+              variant="solid"
+            >
+              See More
+            </Button>
+          </CardFooter>
+
+        </Card >
+        <Card className="hover:shadow-lg md:hover:scale-105 hover:shadow-primary/50 transition-all duration-300 flex flex-col ">
+          <CardContent className="pt-3">
+            <img src={adwordImg} alt="" className="w-full h-48 object-cover rounded-t-lg" />
+          </CardContent>
+
+          <CardTitle>
+            <h2 className="p-6">Mathematics</h2>
+          </CardTitle>
+
+          <CardDescription>
+            <p className="px-6 text-lg flex-grow">Explore the world of numbers, algebra, geometry, and calculus. Develop problem-solving skills and logical thinking through mathematical concepts</p>
+          </CardDescription>
+          <CardContent className="flex gap-2 pt-6 flex-shrink-0">
+            <p>Question: 30 |</p>
+            <p> Time: 60 min.</p>
+          </CardContent>
+          <CardFooter className="flex justify-center">
+            <Button
+              showAnchorIcon
+              as={Link}
+              color="success"
+              href="#"
+              variant="solid"
+            >
+              See More
+            </Button>
+          </CardFooter>
+
+        </Card >
+
+        <Card className="hover:shadow-lg md:hover:scale-105 hover:shadow-primary/50 transition-all duration-300 flex flex-col">
+          <CardContent className="pt-3">
+            <img src={marketingImg} alt="" className="w-full h-48 object-cover rounded-t-lg" />
+          </CardContent>
+
+          <CardTitle>
+            <h2 className="p-6">Geography</h2>
+          </CardTitle>
+
+          <CardDescription>
+            <p className="px-6 text-lg flex-grow">Understand the Earth, its landscapes, and environments. Explore maps, climates, and the impact of human activity on the planet..</p>
+          </CardDescription>
+          <CardContent className="flex gap-2 pt-6 flex-shrink-0">
+            <p>Question: 30 |</p>
+            <p> Time: 60 min.</p>
+          </CardContent>
+          <CardFooter className="flex justify-center">
+            <Button
+              showAnchorIcon
+              as={Link}
+              color="success"
+              href="#"
+              variant="solid"
+            >
+              See More
+            </Button>
+          </CardFooter>
+
+        </Card >
+        <Card className="hover:shadow-lg md:hover:scale-105 hover:shadow-primary/50 transition-all duration-300 flex flex-col ">
+          <CardContent className="pt-3">
+            <img src={seoImg} alt="" className="w-full h-48 object-cover rounded-t-lg" />
+          </CardContent>
+
+          <CardTitle>
+            <h2 className="p-6">English</h2>
+          </CardTitle>
+
+          <CardDescription>
+            <p className="px-6 text-lg flex-grow">Master the art of communication, literature, and writing skills. Learn grammar, vocabulary, and storytelling techniques to excel in English</p>
+          </CardDescription>
+          <CardContent className="flex gap-2 pt-6 flex-shrink-0">
+            <p>Question: 30 |</p>
+            <p> Time: 60 min.</p>
+          </CardContent>
+          <CardFooter className="flex justify-center">
+            <Button
+              showAnchorIcon
+              as={Link}
+              color="success"
+              href="#"
+              variant="solid"
+            >
+              See More
+            </Button>
+          </CardFooter>
+
+        </Card >
+        <Card className="hover:shadow-lg md:hover:scale-105 hover:shadow-primary/50 transition-all duration-300 flex flex-col">
+          <CardContent className="pt-3">
+            <img src={itImg} alt="" className="w-full h-48 object-cover rounded-t-lg" />
+          </CardContent>
+
+          <CardTitle>
+            <h2 className="p-6">Computer Science</h2>
+          </CardTitle>
+
+          <CardDescription>
+            <p className="px-6 text-lg flex-grow">Dive into programming, algorithms, and software development. Learn coding languages, data structures, and how to build applications.</p>
+          </CardDescription>
+          <CardContent className="flex gap-2 pt-6 flex-shrink-0">
+            <p>Question: 30 |</p>
+            <p> Time: 60 min.</p>
+          </CardContent>
+          <CardFooter className="flex justify-center">
+            <Button
+              showAnchorIcon
+              as={Link}
+              color="success"
+              href="#"
+              variant="solid"
+            >
+              See More
+            </Button>
+          </CardFooter>
+
+        </Card >
+        <Card className="hover:shadow-lg md:hover:scale-105 hover:shadow-primary/50 transition-all duration-300 flex flex-col ">
+          <CardContent className="pt-3">
+            <img src={adwordImg} alt="" className="w-full h-48 object-cover rounded-t-lg" />
+          </CardContent>
+
+          <CardTitle>
+            <h2 className="p-6">Mathematics</h2>
+          </CardTitle>
+
+          <CardDescription>
+            <p className="px-6 text-lg flex-grow">Explore the world of numbers, algebra, geometry, and calculus. Develop problem-solving skills and logical thinking through mathematical concepts</p>
+          </CardDescription>
+          <CardContent className="flex gap-2 pt-6 flex-shrink-0">
+            <p>Question: 30 |</p>
+            <p> Time: 60 min.</p>
+          </CardContent>
+          <CardFooter className="flex justify-center">
+            <Button
+              showAnchorIcon
+              as={Link}
+              color="success"
+              href="#"
+              variant="solid"
+            >
+              See More
+            </Button>
+          </CardFooter>
+
+        </Card >
+        <Card className="hover:shadow-lg md:hover:scale-105 hover:shadow-primary/50 transition-all duration-300 flex flex-col">
+          <CardContent className="pt-3">
+            <img src={itImg} alt="" className="w-full h-48 object-cover rounded-t-lg" />
+          </CardContent>
+
+          <CardTitle>
+            <h2 className="p-6">Computer Science</h2>
+          </CardTitle>
+
+          <CardDescription>
+            <p className="px-6 text-lg flex-grow">Dive into programming, algorithms, and software development. Learn coding languages, data structures, and how to build applications.</p>
+          </CardDescription>
+          <CardContent className="flex gap-2 pt-6 flex-shrink-0">
+            <p>Question: 30 |</p>
+            <p> Time: 60 min.</p>
+          </CardContent>
+          <CardFooter className="flex justify-center">
+            <Button
+              showAnchorIcon
+              as={Link}
+              color="success"
+              href="#"
+              variant="solid"
+            >
+              See More
+            </Button>
+          </CardFooter>
+
+        </Card >
+
+     
+
       </CommonWrapper>
-      <CommonWrapper>
-        {isLoading && <Spinner />}
-        {!isLoading && isSuccess && <> Data: {data?.data?.length}</>}
+      <CommonWrapper className=" flex justify-center mt-6">
+     
+      <Pagination initialPage={1} total={10} color="success" />
+  
       </CommonWrapper>
-      <CommonWrapper>
-        <Pagination
-          page={page}
-          onChange={setPage}
-          classNames={{
-            wrapper: "gap-0 overflow-visible h-8 rounded border border-divider",
-            item: "w-8 h-8 text-small rounded-none bg-transparent",
-            cursor:
-              "bg-gradient-to-b shadow-lg from-default-500 to-default-800 dark:from-default-300 dark:to-default-100 text-white font-bold",
-          }}
-          total={Math.ceil(data?.meta?.total / perPage)}
-        />
-      </CommonWrapper>
-      
+
     </>
   );
 };
