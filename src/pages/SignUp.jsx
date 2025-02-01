a78bfc4bd43e51aa1bfb15f0d153d3a1d3798e20
Enamul Hasan Himel <mhasan173013@bscse.uiu.ac.bd>
2025-02-01 18:17:35 +0600

    login and signup

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -5,31 +5,29 @@ import {
   Input,
   Select,
   SelectItem,
-} from '@heroui/react';
-import { ReloadIcon } from '@radix-ui/react-icons';
-import { useContext, useEffect, useState } from 'react';
-import { Controller, useForm } from 'react-hook-form';
-import { Link, useLocation, useNavigate } from 'react-router-dom';
+} from "@heroui/react";
+import { ReloadIcon } from "@radix-ui/react-icons";
+import { useContext, useEffect, useState } from "react";
+import { Controller, useForm } from "react-hook-form";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import {
   Card,
   CardContent,
   CardFooter,
   CardHeader,
-} from '@/components/ui/card';
-import { motion } from 'framer-motion';
-import { EyeSlashFilledIcon } from '@/assets/icons/EyeSlashFilledIcon';
-import { EyeFilledIcon } from '@/assets/icons/EyeFilledIcon';
-import { Player } from '@lottiefiles/react-lottie-player';
-import { Icons } from '@/assets/icons/Icons';
-import { AuthContext } from '@/hooks/AuthContextProvider';
-import usePostMutate from '@/hooks/shared/usePostMutate';
-import Cookies from 'js-cookie';
-import toast from 'react-hot-toast';
+} from "@/components/ui/card";
+import { motion } from "framer-motion";
+import { EyeSlashFilledIcon } from "@/assets/icons/EyeSlashFilledIcon";
+import { EyeFilledIcon } from "@/assets/icons/EyeFilledIcon";
+import { Player } from "@lottiefiles/react-lottie-player";
+import { Icons } from "@/assets/icons/Icons";
+import { AuthContext } from "@/hooks/AuthContextProvider";
+import usePostMutate from "@/hooks/shared/usePostMutate";
+import Cookies from "js-cookie";
+import toast from "react-hot-toast";
 
-import { imageUpload } from '../helpers/cloudinary';
-import ImageSelector from '../others/ImageSelector';
-
-
+import { imageUpload } from "../helpers/cloudinary";
+import ImageSelector from "../others/ImageSelector";
 
 const SignUp = () => {
   const [isLoading, setIsLoading] = useState(false);
@@ -38,7 +36,7 @@ const SignUp = () => {
   const { user, setUser } = useContext(AuthContext);
   useEffect(() => {
     if (user) {
-      navigate('/');
+      navigate("/");
     }
   }, []);
 
@@ -58,29 +56,29 @@ const SignUp = () => {
   const [avatarUrl, setAvatarUrl] = useState();
   const onSuccess = (res) => {
     console.log(res);
-    Cookies.set('user', res?.data?.data?.accessToken, { expires: 30 });
+    Cookies.set("user", res?.data?.data?.accessToken, { expires: 30 });
     setUser(res?.data?.data?.user);
-    toast.success('Successfully Created User');
-    navigate(path || '/dashboard');
+    toast.success("Successfully Created User");
+    navigate(path || "/dashboard");
 
     setIsLoading(false);
   };
   const onError = (err) => {
     console.log(err);
     //     console.log(err?.response?.data?.message);
-    toast.error(err?.response?.data?.message || 'Something went wrong');
+    toast.error(err?.response?.data?.message || "Something went wrong");
     setIsLoading(false);
   };
-  const { mutate } = usePostMutate('/users/', onSuccess, onError);
+  const { mutate } = usePostMutate("/users/", onSuccess, onError);
 
   const onSubmit = async (userData) => {
     setIsLoading(true);
     if (!userData.avatar) {
       setIsLoading(false);
 
-      return setError('avatar', {
-        type: 'manual',
-        message: 'Image is required.',
+      return setError("avatar", {
+        type: "manual",
+        message: "Image is required.",
       });
     }
     console.log(userData);
@@ -89,7 +87,7 @@ const SignUp = () => {
 
   const toggleVisibility = () => setIsVisible(!isVisible);
   const handleFileChange = async (e) => {
-    toast.loading('Uploading Image Please Wait...');
+    toast.loading("Uploading Image Please Wait...");
 
     const selectedFile = e.target.files?.[0];
     if (selectedFile) {
@@ -98,17 +96,16 @@ const SignUp = () => {
         const { secure_url } = imageUrl;
         console.log(secure_url);
 
-        setValue('avatar', secure_url);
-        clearErrors('avatar');
+        setValue("avatar", secure_url);
+        clearErrors("avatar");
 
         setAvatarUrl(secure_url);
-        // clearErrors('imageUrl');
+        clearErrors("imageUrl");
       } catch (e) {
         console.log(e);
       }
     }
   };
-
 
   return (
     <motion.div
@@ -118,9 +115,9 @@ const SignUp = () => {
       exit="exit"
       className="  relative  min-h-screen"
     >
-      <Link to={'/'}>
+      {/* <Link to={'/'}>
         <Icons.logoICon className=" hidden md:flex absolute  text-danger  top-10 left-10" />
-      </Link>
+      </Link> */}
       {/* <Helmet>
         <title>Sign In | Jobify</title>
         <link rel="canonical" href="https://jobify-bd6c2.web.app/" />
@@ -128,12 +125,17 @@ const SignUp = () => {
 
       <div className=" w-full mx-auto md:grid grid-cols-3  h-full   items-center justify-center">
         <div className="flex-1 hidden md:flex min-h-full col-span-2   justify-center items-center  h-full w-full dark:bg-darkish    bg-no-repeat  mx-auto  ">
-          <Player
+          {/* <Player
             autoplay
             loop
             src="https://lottie.host/a1a5f6ba-fffd-4887-a289-638b75651637/gFaYX8uQLu.json"
-            style={{ height: '500px', width: '500px' }}
-          ></Player>
+            style={{ height: "500px", width: "500px" }}
+          ></Player> */}
+          <img
+            src="/src/assets/8609249_3966070.jpg"
+            alt=""
+            style={{ height: "500px", width: "500px" }}
+          />
         </div>
         <div className="flex-1 w-full  flex justify-center items-center mx-auto">
           <Card className="bg-transparent border-none  ">
@@ -142,8 +144,8 @@ const SignUp = () => {
                 Get started absolutely free
               </p>
               <p className="text-sm font-medium">
-                Already have an account?{' '}
-                <Link to={'/login'} className="text-danger hover:underline">
+                Already have an account?{" "}
+                <Link to={"/login"} className="text-danger hover:underline">
                   Sign In
                 </Link>
               </p>
@@ -160,7 +162,7 @@ const SignUp = () => {
                         isBordered
                         size="md"
                         classNames={{
-                          base: ' h-[50px] ',
+                          base: " h-[50px] ",
                         }}
                         radius="sm"
                         src={avatarUrl}
@@ -172,7 +174,7 @@ const SignUp = () => {
                         isBordered
                         size="md"
                         classNames={{
-                          base: ' h-[50px] ',
+                          base: " h-[50px] ",
                         }}
                         radius="sm"
                         icon={<AvatarIcon />}
@@ -182,10 +184,10 @@ const SignUp = () => {
 
                   <div className="w-full">
                     <Controller
-                      name="name"
+                      name="First Name"
                       control={control}
                       defaultValue=""
-                      rules={{ required: ' Name is required' }}
+                      rules={{ required: "First Name is required" }}
                       render={({ field }) => (
                         <div>
                           <Input
@@ -193,23 +195,44 @@ const SignUp = () => {
                             type="text"
                             isInvalid={errors.name ? true : false}
                             classNames={{
-                              errorMessage: 'text-left',
+                              errorMessage: "text-left",
                             }}
                             errorMessage={errors.name && errors.name.message}
-                            label="Name"
-                            variant={'bordered'}
+                            label="First Name"
+                            variant={"bordered"}
                           />
                         </div>
                       )}
                     />
                   </div>
                 </div>
+                <Controller
+                  name="Last Name"
+                  control={control}
+                  defaultValue=""
+                  rules={{ required: "Last Name is required" }}
+                  render={({ field }) => (
+                    <div>
+                      <Input
+                        {...field}
+                        type="text"
+                        isInvalid={errors.email ? true : false}
+                        classNames={{
+                          errorMessage: "text-left",
+                        }}
+                        errorMessage={errors.email && errors.email.message}
+                        label="Last Name"
+                        variant={"bordered"}
+                      />
+                    </div>
+                  )}
+                />
 
                 <Controller
                   name="email"
                   control={control}
                   defaultValue=""
-                  rules={{ required: ' Email is required' }}
+                  rules={{ required: " Email is required" }}
                   render={({ field }) => (
                     <div>
                       <Input
@@ -217,20 +240,20 @@ const SignUp = () => {
                         type="email"
                         isInvalid={errors.email ? true : false}
                         classNames={{
-                          errorMessage: 'text-left',
+                          errorMessage: "text-left",
                         }}
                         errorMessage={errors.email && errors.email.message}
                         label="Email"
-                        variant={'bordered'}
+                        variant={"bordered"}
                       />
                     </div>
                   )}
                 />
-                <Controller
+                {/* <Controller
                   name="bloodGroup"
                   control={control}
                   defaultValue=""
-                  rules={{ required: 'Role is required' }}
+                  rules={{ required: "Role is required" }}
                   render={({ field }) => (
                     <div>
                       <Select
@@ -243,7 +266,7 @@ const SignUp = () => {
                         }
                         variant="bordered"
                       >
-                        {['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'].map(
+                        {["A+", "A-", "B+", "B-", "AB+", "AB-", "O+", "O-"].map(
                           (item) => (
                             <SelectItem key={item} value={item}>
                               {item}
@@ -253,32 +276,31 @@ const SignUp = () => {
                       </Select>
                     </div>
                   )}
-                />
-            
+                /> */}
 
                 <Controller
                   name="password"
                   control={control}
                   defaultValue=""
                   rules={{
-                    required: 'Password is required',
+                    required: "Password is required",
                     minLength: {
                       value: 6,
-                      message: 'Password is incorrect',
+                      message: "Password is incorrect",
                     },
                   }}
                   render={({ field }) => (
                     <div>
                       <Input
                         {...field}
-                        type={isVisible ? 'text' : 'password'}
-                        variant={'bordered'}
+                        type={isVisible ? "text" : "password"}
+                        variant={"bordered"}
                         isInvalid={errors.password ? true : false}
                         errorMessage={
                           errors.password && errors.password.message
                         }
                         classNames={{
-                          errorMessage: 'text-left',
+                          errorMessage: "text-left",
                         }}
                         label="Password"
                         endContent={
@@ -304,15 +326,15 @@ const SignUp = () => {
                   control={control}
                   defaultValue=""
                   rules={{
-                    required: 'Confirm Password is required',
+                    required: "Confirm Password is required",
                     validate: (value) =>
-                      value === watch('password') || 'Passwords do not match',
+                      value === watch("password") || "Passwords do not match",
                   }}
                   render={({ field }) => (
                     <div>
                       <Input
                         {...field}
-                        type={isVisible ? 'text' : 'password'}
+                        type={isVisible ? "text" : "password"}
                         isInvalid={errors.confirmPassword ? true : false}
                         errorMessage={
                           errors.confirmPassword &&
@@ -331,9 +353,9 @@ const SignUp = () => {
                             )}
                           </button>
                         }
-                        variant={'bordered'}
+                        variant={"bordered"}
                         classNames={{
-                          errorMessage: 'text-left',
+                          errorMessage: "text-left",
                         }}
                         label="Confirm Password"
                       />
@@ -363,7 +385,7 @@ const SignUp = () => {
                 </Button>
               </form>
               <div className="text-muted-foreground text-tiny text-center mt-5">
-                By signing up, I agree to{' '}
+                By signing up, I agree to{" "}
                 <span className="text-primary underline cursor-pointer">
                   Terms of Service
                 </span>
@@ -384,12 +406,12 @@ export default SignUp;
 const containerVariants = {
   hidden: {
     opacity: 0,
-    x: '-100vh',
+    x: "-100vh",
   },
   exit: {
-    x: '100vh',
+    x: "100vh",
     transition: {
-      ease: 'easeInOut',
+      ease: "easeInOut",
     },
   },
   visible: {
@@ -398,7 +420,7 @@ const containerVariants = {
     transition: {
       duration: 0.5,
 
-      type: 'spring',
+      type: "spring",
     },
   },
 };
